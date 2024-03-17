73394c9df7ab88dfbe74920188f9480a51c7e9e6
havrob29 <havrobert@gmail.com>
2024-03-17 02:36:52 +0100

    cors header

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	httpHandler := http.NewServeMux()
-	fmt.Println(httpHandler)
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving on %v\n", srv.Addr)
+
+	srv.ListenAndServe()
+
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
