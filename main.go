1f57c3b95ce4f7a3001a24a5c39722a8f55339a7
havrob29 <havrobert@gmail.com>
2024-03-18 14:19:11 +0100

    commit

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -31,31 +30,4 @@ func main() {
 
 	log.Printf("Serving files from %s on %s\n", http.Dir("."), srv.Addr)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (apiCfg *apiConfig) adminMetrics(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.WriteHeader(405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", apiCfg.fileserverHits)))
-}
-
-func (apiCfg *apiConfig) resetNumberRequests(w http.ResponseWriter, r *http.Request) {
-	apiCfg.fileserverHits = 0
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(200)
-	w.Write([]byte("Hits reset to 0"))
 }
